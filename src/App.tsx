5d159a20aa3dc92edeb8499f520274d75ea3d0c3
Pedro Botega <botegapedro@gmail.com>
2024-07-30 15:29:34 +0100

    creates initial loading log messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './App.module.scss';
 import { RoutesPages } from './routes/Routes';
 import yorhaLogo from "./assets/yorha-opacity-logo.png"
@@ -23,6 +23,77 @@ const LoadingDots = () => {
     }, 500)
   }, [dots])
   return <span>{dots}</span>
+}
+
+export const Typer = ({ receivedText, callBack, speed = 100 }) => {
+  const [typingText, setTypingText] = useState<string>("")
+
+  function startTyping() {
+    if (typingText.length < receivedText.length) {
+      setTimeout(() => {
+        setTypingText(prev => prev += receivedText.charAt(typingText.length));
+      }, speed);
+    }
+    else callBack()
+
+  }
+
+  useEffect(startTyping, [receivedText, typingText])
+
+  if (!receivedText) return <></>
+
+  return <p>{typingText}</p>
+}
+
+const LoadingLogs = ({ removeSpeed = 1000 }) => {
+  const [loadingCompleted, setLoadingCompleted] = useState(false)
+
+  const messages: string[] = [
+    "Commencing System Check",
+    "Memory Unit: Green",
+    "Initializing Tactics Log",
+    "Loading Geographic Data",
+    "Vitals: Green",
+    "Remaining MP: 100%",
+    "Black Box Temperature: Normal",
+    "Black Box Internal Pressure: Normal",
+    "Activating IFF",
+    "Activating FCS",
+    "Initializing Pod Connection",
+    "Launching DBU Setup",
+    "Activating Inertia Control System",
+    "Activating Environmental Sensors",
+    "Equipment Authentication: Complete",
+    "Equipment Status: Green",
+    "All Systems Green",
+    "Combat Preparations Complete"
+  ];
+
+  const [waitingListRender, setWaitingListRender] = useState([messages[0]])
+
+  const updateRenderList = (i: number) => {
+    if (!messages[i + 1]) return
+    setWaitingListRender(prev => [...prev, messages[i + 1]])
+  }
+
+  const removeComponent = () => {
+    if (waitingListRender.length < messages.length)
+      return
+
+    setTimeout(() => {
+      setLoadingCompleted(true)
+    }, removeSpeed)
+  }
+
+  useEffect(removeComponent, [waitingListRender])
+
+  if (loadingCompleted) return <></>
+
+  return <>
+    {waitingListRender.map((message, i) => (
+      <Typer key={message} callBack={() => updateRenderList(i)} receivedText={message} />
+    ))}
+  </>
 }
 
 const LoadingScreen = () => {
@@ -52,6 +123,7 @@ const LoadingScreen = () => {
       </div>
       <SpinLoadginIcon />
     </header>
+    <LoadingLogs removeSpeed={10000} />
   </section >
 }
 
