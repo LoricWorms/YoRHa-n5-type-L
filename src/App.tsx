13f18295a0aefb80169a13cdc1b4c3924f4d6ba7
Pedro Botega <botegapedro@gmail.com>
2024-07-31 12:27:11 +0100

    Gets original App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styles from './App.module.scss';
 import { RoutesPages } from './routes/Routes';
-import { LoadingDots, LoadingLogs, SpinLoadingIcon } from './components';
-import { Loading } from './pages';
 
 function App() {
   return (
-    <div className={styles.App}>
-      {/* <NavbarModule/>
+      <div className={styles.App}>
+        {/* <NavbarModule/>
         <hr/>
-        <div className={styles.dottedspaced}/>
+        <div className={styles.dottedspaced}/> */}
         <RoutesPages/>
-         */}
-      <Loading />
-    </div>
+      </div>
   );
 }
 
